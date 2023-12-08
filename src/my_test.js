28207cfda92420d0a93fa2ed769dcf9c7babedea
vladimir.grinenko <S1vjycnhbr>
2023-12-08 17:28:46 +0300

    feat: hanoi-tower finish

--- src/my_test.js
+++ src/my_test.js
@@ -1,36 +1,13 @@
-function getSeason(date) {
-  if (!date) {
-    return 'Unable to determine the time of year!';
-  }
+function calculateHanoi(disksNumber, turnsSpeed) {
+  const turns = Math.pow(2, disksNumber) - 1;
+  const seconds = Math.floor((turns * 3600) / turnsSpeed);
 
-  if (!(date instanceof Date) || Object.getOwnPropertyNames(date).length) {
-    throw Error('Invalid date!');
-  }
-
-  const month = date.getMonth();
-  const spring = [2, 3, 4];
-  const summer = [5, 6, 7];
-  const autumn = [8, 9, 10];
-
-  if (spring.includes(month)) {
-    return 'spring';
-  } else if (summer.includes(month)) {
-    return 'summer';
-  } else if (autumn.includes(month)) {
-    return 'autumn';
-  } else {
-    return 'winter';
-  }
+  return {
+    turns,
+    seconds,
+  };
 }
 
-const fakeDate = {
-  toString() {
-    return Date.prototype.toString.call(new Date());
-  },
-  [Symbol.toStringTag]: 'Date',
-};
-
-Object.setPrototypeOf(fakeDate, Object.getPrototypeOf(new Date()));
-
-console.log(getSeason(fakeDate));
-// console.log(fakeDate.hasOwnProperty('getTime'));
+const disksNumber = 9;
+const turnsSpeed = 4308;
+console.log(calculateHanoi(disksNumber, turnsSpeed));
