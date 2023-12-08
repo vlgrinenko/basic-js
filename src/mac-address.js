e96dce386218bba256385ab492f9d12349e6ad42
vladimir.grinenko <S1vjycnhbr>
2023-12-09 02:07:08 +0300

    feat: finish mac-address and matrix

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,10 +14,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  const groups = n.split('-');
+  let isMac = true;
+
+  if (groups.length !== 6) {
+    return false;
+  }
+
+  groups.forEach((group) => {
+    if (group.length !== 2 || !/^[0-9a-fA-F{2}]/.test(group)) {
+      isMac = false;
+    }
+  });
+
+  return isMac;
 }
 module.exports = {
-  isMAC48Address
+  isMAC48Address,
 };
--- src/my_test.js
+++ src/my_test.js
@@ -1,8 +1,18 @@
-function getEmailDomain(email) {
-  const arr = email.split('@');
+function isMAC48Address(n) {
+  const groups = n.split('-');
+  let isMac = true;
 
-  return arr[arr.length - 1];
+  if (groups.length !== 6) {
+    return false;
+  }
+
+  groups.forEach((group) => {
+    if (group.length !== 2 || !/^[0-9a-fA-F{2}]/.test(group)) {
+      isMac = false;
+    }
+  });
+
+  return isMac;
 }
 
-console.log(getEmailDomain('very.unusual.@.unusual.com@usual.com'));
-// '4a4t'
+console.log(isMAC48Address('00-1B-63-84-45-E6'));
