649417e573b599ccb73861c8f5b4c1464079f682
vladimir.grinenko <S1vjycnhbr>
2023-12-08 20:48:34 +0300

    feat: finish transform-array

--- src/my_test.js
+++ src/my_test.js
@@ -1,13 +1,43 @@
-function calculateHanoi(disksNumber, turnsSpeed) {
-  const turns = Math.pow(2, disksNumber) - 1;
-  const seconds = Math.floor((turns * 3600) / turnsSpeed);
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw Error("'arr' parameter must be an instance of the Array!");
+  }
+  const discardNext = '--discard-next';
+  const discardPrev = '--discard-prev';
+  const doubleNext = '--double-next';
+  const doublePrev = '--double-prev';
+  const newArr = [];
 
-  return {
-    turns,
-    seconds,
-  };
+  for (let i = 0; i < arr.length; i += 1) {
+    switch (arr[i]) {
+      case discardNext:
+        i += 1;
+        break;
+      case discardPrev:
+        if (i > 0 && arr[i - 2] !== discardNext) {
+          newArr.pop();
+        }
+        break;
+      case doubleNext:
+        if (i < arr.length - 1) {
+          newArr.push(arr[i + 1]);
+        }
+        break;
+      case doublePrev:
+        if (i > 0 && arr[i - 2] !== discardNext) {
+          newArr.push(arr[i - 1]);
+        }
+        break;
+      default:
+        newArr.push(arr[i]);
+        break;
+    }
+  }
+
+  return newArr;
 }
 
-const disksNumber = 9;
-const turnsSpeed = 4308;
-console.log(calculateHanoi(disksNumber, turnsSpeed));
+console.log(
+  transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5])
+);
+// output: [1, 2, 3, 4, 5]
--- src/transform-array.js
+++ src/transform-array.js
@@ -3,21 +3,55 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw Error("'arr' parameter must be an instance of the Array!");
+  }
+  const discardNext = '--discard-next';
+  const discardPrev = '--discard-prev';
+  const doubleNext = '--double-next';
+  const doublePrev = '--double-prev';
+  const newArr = [];
+
+  for (let i = 0; i < arr.length; i += 1) {
+    switch (arr[i]) {
+      case discardNext:
+        i += 1;
+        break;
+      case discardPrev:
+        if (i > 0 && arr[i - 2] !== discardNext) {
+          newArr.pop();
+        }
+        break;
+      case doubleNext:
+        if (i < arr.length - 1) {
+          newArr.push(arr[i + 1]);
+        }
+        break;
+      case doublePrev:
+        if (i > 0 && arr[i - 2] !== discardNext) {
+          newArr.push(arr[i - 1]);
+        }
+        break;
+      default:
+        newArr.push(arr[i]);
+        break;
+    }
+  }
+
+  return newArr;
 }
 
 module.exports = {
-  transform
+  transform,
 };
