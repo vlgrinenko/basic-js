e2d3857aec62ef70f4fdc21c7cefef7e6adb1d4a
vladimir.grinenko <S1vjycnhbr>
2023-12-09 00:14:01 +0300

    feat: finish character-count

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,11 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const array1 = s1.split('');
+  const array2 = s2.split('');
+
+  const matchesArray = array1.filter((char) => {
+    const indexInArray2 = array2.indexOf(char);
+
+    if (indexInArray2 !== -1) {
+      array2.splice(indexInArray2, 1);
+      return true;
+    }
+
+    return false;
+  });
+
+  return matchesArray.length;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
--- src/my_test.js
+++ src/my_test.js
@@ -1,84 +1,19 @@
-class VigenereCipheringMachine {
-  constructor(isDirect = true) {
-    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    this.isDirect = isDirect;
-  }
+function getCommonCharacterCount(s1, s2) {
+  const array1 = s1.split('');
+  const array2 = s2.split('');
 
-  encrypt(message, key) {
-    if (
-      typeof message !== 'string' ||
-      !message ||
-      typeof key !== 'string' ||
-      !key
-    ) {
-      throw Error('Incorrect arguments!');
+  const matchesArray = array1.filter((char) => {
+    const indexInArray2 = array2.indexOf(char);
+
+    if (indexInArray2 !== -1) {
+      array2.splice(indexInArray2, 1);
+      return true;
     }
 
-    message = message.toUpperCase();
-    key = key.toUpperCase();
+    return false;
+  });
 
-    let result = '';
-    let index = 0;
-
-    for (let i = 0; i < message.length; i += 1) {
-      const charMessage = message[i];
-
-      if (this.alphabet.includes(charMessage)) {
-        const charKey = key[index % key.length];
-
-        const messageIndex = this.alphabet.indexOf(charMessage);
-        const keyIndex = this.alphabet.indexOf(charKey);
-
-        const newIndex = (messageIndex + keyIndex + 26) % 26;
-
-        result += this.alphabet[newIndex];
-        index += 1;
-      } else {
-        result += charMessage;
-      }
-    }
-
-    return this.isDirect ? result : result.split('').reverse().join('');
-  }
-  decrypt(message, key) {
-    if (
-      typeof message !== 'string' ||
-      !message ||
-      typeof key !== 'string' ||
-      !key
-    ) {
-      throw Error('Incorrect arguments!');
-    }
-
-    message = message.toUpperCase();
-    key = key.toUpperCase();
-
-    let result = '';
-    let index = 0;
-
-    for (let i = 0; i < message.length; i += 1) {
-      const charMessage = message[i];
-
-      if (this.alphabet.includes(charMessage)) {
-        const charKey = key[index % key.length];
-
-        const messageIndex = this.alphabet.indexOf(charMessage);
-        const keyIndex = this.alphabet.indexOf(charKey);
-
-        const newIndex = (messageIndex - keyIndex + 26) % 26;
-
-        result += this.alphabet[newIndex];
-        index += 1;
-      } else {
-        result += charMessage;
-      }
-    }
-
-    return this.isDirect ? result : result.split('').reverse().join('');
-  }
+  return matchesArray.length;
 }
 
-const directMachine = new VigenereCipheringMachine();
-
-console.log(directMachine.encrypt('attack at dawn!', 'alphonse'));
-// reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
+console.log(getCommonCharacterCount('aabcc', 'adcaa'));
